8a387071eca0a0f08419e2c9bcbe38476ed09ea4
Dorian Kita <dorian.kita90@gmail.com>
2023-09-14 20:27:54 +0200

    added working sliders to change photo filters

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const brightnessBar = document.getElementById('brightness-bar');
+    const contrastBar = document.getElementById('contrast-bar');
+    const saturationBar = document.getElementById('saturation-bar');
+    const hueBar = document.getElementById('hue-bar');
+    const blurBar = document.getElementById('blur-bar');
+    const exampleImage = document.querySelector('.example-image');
+
+    const updatesFilters = () => {
+        exampleImage.style.filter = `
+        brightness(${brightnessBar.value}%)
+        contrast(${contrastBar.value}%)
+        saturate(${saturationBar.value}%)
+        hue-rotate(${hueBar.value}deg)
+        blur(${blurBar.value}px)
+        `;
+    };   
+
+    [brightnessBar,contrastBar,saturationBar,hueBar,blurBar].forEach((bar) => {
+        bar.addEventListener('input', updatesFilters);
+    });
+
+});
