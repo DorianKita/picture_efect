d8e41c2a2803402ec91ce0a80e8e1b6debb803d5
Dorian Kita <dorian.kita90@gmail.com>
2023-09-14 20:47:15 +0200

    changed styles to use css variables

--- index.js
+++ index.js
@@ -6,16 +6,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const hueBar = document.getElementById('hue-bar');
     const blurBar = document.getElementById('blur-bar');
     const exampleImage = document.querySelector('.example-image');
+    const root = document.documentElement;
 
-    const updatesFilters = () => {
-        exampleImage.style.filter = `
-        brightness(${brightnessBar.value}%)
-        contrast(${contrastBar.value}%)
-        saturate(${saturationBar.value}%)
-        hue-rotate(${hueBar.value}deg)
-        blur(${blurBar.value}px)
-        `;
-    };   
+    // const updatesFilters = () => {
+    //     exampleImage.style.filter = `
+    //     brightness(${brightnessBar.value}%)
+    //     contrast(${contrastBar.value}%)
+    //     saturate(${saturationBar.value}%)
+    //     hue-rotate(${hueBar.value}deg)
+    //     blur(${blurBar.value}px)
+    //     `;
+    // };   
+
+      const updatesFilters = () => {
+        root.style.setProperty('--brightness', brightnessBar.value + '%');
+        root.style.setProperty('--contrast', contrastBar.value + '%');
+        root.style.setProperty('--saturation', saturationBar.value + '%');
+        root.style.setProperty('--hue', hueBar.value + 'deg');
+        root.style.setProperty('--blur', blurBar.value + 'px');
+    };  
 
     [brightnessBar,contrastBar,saturationBar,hueBar,blurBar].forEach((bar) => {
         bar.addEventListener('input', updatesFilters);
